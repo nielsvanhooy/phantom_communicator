e165c0ac7133a421ba117e9b191f80e1e877c40b
hooij804 <niels.vanhooij@kpn.com>
2024-05-06 15:29:30 +0200

    .

--- phantom_communicator/command_blocks/commands/cisco_iosxe.py
+++ phantom_communicator/command_blocks/commands/cisco_iosxe.py
@@ -12,6 +12,7 @@ def show_ver(*args, **kwargs) -> Command:
 def setup_session() -> Command:
     return Command("terminal length 0")
 
-@command(name="show_version", vendor="cisco", os="iosxe")
+
+@command(name="show_software", vendor="cisco", os="iosxe")
 def show_software(*args, **kwargs) -> list:
     return [Command("show ver"), SNMPCommand(".1.3.6.1.4.1.9.2.1.73.0")]
