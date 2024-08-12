38e7445c3ce530ea9b7bbe39e4c4e702a12a86e7
hooij804 <niels.vanhooij@kpn.com>
2024-08-12 14:28:22 +0200

    .

--- phantom_communicator/command_blocks/parsers/iosxe/show_featureset.py
+++ phantom_communicator/command_blocks/parsers/iosxe/show_featureset.py
@@ -7,4 +7,4 @@ from phantom_communicator.command_blocks.decorators import command_or_parse
 @command_or_parse(name=commands.SHOW_FEATURESET, vendor=commands.CISCO, os="iosxe", type="parse_command")
 def parse_show_featureset(command_results) -> dict[str, Any]:
     data = command_results.result
-    return data.replace('"', "")
+    return {"featureset": data.replace('"', "")}
